89fe92447f7b9acbbfcdb323193d5593545921e4
sgupta59 <sgupta59_github>
2015-09-24 23:02:23 -0500

    TSP Brute Force

--- scratch/src/backtracking/HamiltonianPath.java
+++ scratch/src/backtracking/HamiltonianPath.java
@@ -15,54 +15,7 @@ package backtracking;
  */
 public class HamiltonianPath
 {
-    public static boolean promising(int[][] W, int[] vindex, int i)
-    {
-        int j;
-        boolean status;
-
-        int prevvid = vindex[i-1]-1;
-        int cvid = vindex[i]-1;
-        if (i == W.length - 1 &&   W[vindex[W.length - 1]-1] [vindex [0]-1] ==0)
-            status = false;                                           // First vertex must be adjacent
-        else if (i > 0 &&  W[vindex[i - 1]-1] [vindex [i]-1] ==0)
-            status = false;                                               // to last. ith vertex must
-        else{                                                            // be adjacent to (i - 1) st.
-            status = true;
-           j = 1;
-           while (j < i && status){                                      // Check if vertex is
-               if (vindex[i] == vindex [j])                              // already selected.
-                   status = false;
-               j++;
-              }
-           }
-        return status;
-    }
-    public static void hamiltonianAllPaths (int[][] g, int[] path, int i)
-    {
-        int j;
-
-        if (promising (g, path, i))
-        {
-          if (i == g.length - 1)
-          {
-              for (int idx = 0; idx < g.length; ++idx)
-                  System.out.print(" " + path[idx]);
-              System.out.println("");
-          }
-          else
-          {
-             for (j = 2; j <= g.length; j++)
-             {             // Try all vertices as
-
-                 path [i + 1] = j;         // next one.
-                 hamiltonianAllPaths (g, path, i + 1);
-                 System.out.println("Processed Vertex: " + j);
-             }
-          }
-        }
-    }
-
-    public static boolean isPromising(int[][] W, int[] path,int pos,  int v)
+   public static boolean isPromising(int[][] W, int[] path,int pos,  int v)
     {
         /** check if this vertex is adjacent vertex of the previously added vertex */
         int prev = pos-1;
@@ -108,7 +61,7 @@ public class HamiltonianPath
         for (int idx = 0; idx < g.length; ++idx)
             path[idx] = -1;
         path[0] = 1;
-        hamiltonianAllPaths(g, path, 0);
+         
         path[0] = 1;
         if (hamiltonianCycleUtil(g, path, 1) == false)
         {
---
+++ scratch/src/backtracking/TravellingSalesmanBruteForce.java
@@ -0,0 +1,113 @@
+package backtracking;
+/**
+ *  http://cs.indstate.edu/~zeeshan/aman.pdf
+ *  A brute force implementation of the travelling salesman problem.
+ *  Calculate the path lengths of all hamiltonian circuits and find the smallest path.
+ *  
+ *  Use an adjacency matrix to get the paths.
+ *  An array is used to store the paths 
+ * @author kg
+ *
+ */
+public class TravellingSalesmanBruteForce {
+
+	int[][] g = null;
+	// the path itself.
+	int path[] = null;
+	// current position for the vertex that will be added to the path
+	int idx = 0;
+	// shortest path
+	int stpath = Integer.MAX_VALUE;
+	public TravellingSalesmanBruteForce(int[][] graph)
+	{
+		g = graph;
+		path = new int[g.length];
+		for (int i = 0; i < path.length; ++i)
+			path[i] = -1;
+		// starting from vertex 0.
+		path[0] = 0;
+		idx = 1;
+	}
+	/**
+	 *  The path length
+	 * @return
+	 */
+	private int getPathLength()
+	{
+		int len = 0;
+		for  (int i = 0; i < path.length-1; ++i)
+		{
+			int u = path[i];
+			int v = path[i+1];
+			len = len + g[u][v];
+		}
+		len = len + g[path[path.length-1]][path[0]];
+		return len;
+	}
+	private void printPath()
+	{
+		System.out.print(path[0]);
+		for (int i = 1; i < path.length; ++i)
+			System.out.print(" -> " + path[i] );
+		System.out.println(" -> " + path[0]);
+	}
+	
+	/*
+	 * This is a solution if there are no -1 in the path, i.e. all vertices have been visited.
+	 */
+	private boolean isSolution()
+	{
+		for (int i = 0; i < path.length; ++i)
+			if (path[i] == -1)
+				return false;
+		return true;
+	}
+	
+	/*
+	 * A vertex is valid if it is not already in the path.
+	 */
+	private boolean isValid(int u)
+	{
+		for (int i = 0; i < path.length; ++i)
+			if (path[i] == u)
+				return false;
+		return true;
+	}
+	public void shortestPath(int pos)
+	{
+		// if this is a solution, then calculate the path length and return
+		if (isSolution()) 
+		{
+			printPath();
+			int len = getPathLength();
+			if (len < stpath)
+			{
+				stpath = len;
+			}
+			return;
+		}
+		// for each neighbor of position pos
+		int u = pos;
+		for (int v = 0; v < g.length; ++v) 
+		{
+			if (isValid(v) && u != v) {
+				// depth first search, mark the vertex
+				path[idx++] = v;
+				shortestPath(v);
+				// unmark the vertex
+				path[--idx] = -1;
+			}
+		}
+	}
+	public static void main(String[] args)
+	{
+		int[][] graph = {
+				{ 0, 3, 5, 2 },
+				{3, 0, 9, 5 },
+				{5, 9, 0, 1 },
+				{2, 5, 1, 0 }
+		};
+		TravellingSalesmanBruteForce tsp = new TravellingSalesmanBruteForce(graph);
+		tsp.shortestPath(0);
+	}
+}
